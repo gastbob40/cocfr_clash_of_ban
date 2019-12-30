6b9ee62a0b8850f6301cf406272d5efd57406117
gastbob40 <gastbob40@gmail.com>
2019-12-30 23:17:18 +0100

    feat (post_restriction): add model

--- src/models/post_resctriction.py
+++ src/models/post_resctriction.py
@@ -10,13 +10,12 @@ with open("run/config/config.yml", 'r') as stream:
     )
 
 
-class Donation:
+class PostRestriction:
     id: int
     user_id: int
-    amount: int
+    channel_id: int
     start_time: datetime.date
     end_time: datetime.date
-    is_active: bool
 
     def __init__(self, **kwargs):
         """
@@ -26,37 +25,33 @@ class Donation:
         if 'data' in kwargs:
             self.id = int(kwargs['data']['id'])
             self.user_id = int(kwargs['data']['user_id'])
-            self.amount = int(kwargs['data']['amount'])
+            self.channel_id = int(kwargs['data']['amount'])
             self.start_time = datetime.datetime.strptime(kwargs['data']['start_time'], '%Y-%m-%dT%H:%M:%SZ')
             self.end_time = datetime.datetime.strptime(kwargs['data']['end_time'], '%Y-%m-%dT%H:%M:%SZ')
-            self.is_active = kwargs['data']['is_active']
         else:
             self.id = -1
             self.user_id = -1
-            self.amount = -1
+            self.channel_id = -1
             self.start_time = datetime.datetime.now()
             self.end_time = datetime.datetime.now()
-            self.is_active = True
 
     def save(self):
-        state, r = api_manager.post_data('donations',
+        state, r = api_manager.post_data('post-restrictions',
                                          user_id=str(self.user_id),
-                                         amount=self.amount,
+                                         channel_id=str(self.amount),
                                          start_time=self.start_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
-                                         end_time=self.end_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
-                                         is_active=self.is_active)
+                                         end_time=self.end_time.strftime('%Y-%m-%dT%H:%M:%SZ'))
         if state:
             self.id = r['id']
 
     def update(self):
-        state, r = api_manager.edit_data('donations',
+        state, r = api_manager.edit_data('post-restrictions',
                                          self.id,
                                          user_id=str(self.user_id),
-                                         amount=self.amount,
+                                         channel_id=str(self.amount),
                                          start_time=self.start_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
-                                         end_time=self.end_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
-                                         is_active=self.is_active)
+                                         end_time=self.end_time.strftime('%Y-%m-%dT%H:%M:%SZ'))
 
     def delete(self):
-        state, r = api_manager.delete_data('donations',
+        state, r = api_manager.delete_data('post-restrictions',
                                            self.id)
