408d57f524faba385a67ab880bcb7277d85aa84e
gastbob40 <gastbob40@gmail.com>
2020-01-04 22:43:18 +0100

    fix (mention_moderator): pass permission to lvl 7

--- src/events_handler/on_message/miscs/mention_moderator.py
+++ src/events_handler/on_message/miscs/mention_moderator.py
@@ -8,18 +8,28 @@ async def mention_moderator(client: discord.Client, message: discord.Message, co
 
     for role in message.role_mentions:
         if role.id == 490155585371766814:
-            barbar_role: discord.Role = message.guild.get_role(280457976719081491)
+            archer_role: discord.Role = message.guild.get_role(497451928058462221)
 
-            if not barbar_role in message.author.roles:
+            if not archer_role in message.author.roles:
                 return await message.channel.send(
                     embed=EmbedsManager.error_embed(
-                        f"Erreur, il faut être niveau 5 (soit {barbar_role.name}) afin de mentionner un modérateur.\n"
+                        f"Erreur, il faut être niveau 5 (soit {archer_role.name}) afin de mentionner un modérateur.\n"
                         f"Vous pouvez cependant vous adresser à un modérateur connecté."
                     )
                         .set_footer(icon_url=client.user.avatar_url, text='Made By Gastbob40')
                 )
 
             await client.get_channel(config['channels']['moderator']).send(
+                embed=EmbedsManager.sanction_embed(
+                    f"{message.author.name}#{message.author.discriminator} vient de mentionner les modérateurs."
+                )
+                    .add_field(name='Message', value=message.content, inline=True)
+                    .add_field(name='Salon', value=message.channel.mention, inline=True)
+                    .add_field(name="Lien", value=f"[{message.jump_url}]({message.jump_url})", inline=False)
+                    .set_footer(icon_url=client.user.avatar_url, text='Made By Gastbob40')
+            )
+
+            await client.get_channel(config['channels']['log_reactions']).send(
                 embed=EmbedsManager.sanction_embed(
                     f"{message.author.name}#{message.author.discriminator} vient de mentionner les modérateurs."
                 )
